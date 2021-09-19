1cac63add799c9ae8dac1281b077d77859cd003a
y-kouhei9 <kouheiyamamoto777dev@gmail.com>
2021-09-19 19:51:56 +0900

    Add input limit

--- numbering_game.c
+++ numbering_game.c
@@ -16,29 +16,40 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+# define MAX_STAGE   (10)		// 入力制限回数
+# define RAND_RANGE	 (1000)		// 乱数の範囲
+
 
 int main(void)
 {
-	int no;
-	int ans;
+	int no;						// 入力された値
+	int ans;					// 当てる数
+	int remain = MAX_STAGE;		// 残入力回数
 
+	// タネの初期化と範囲の設定
 	srand(time(NULL));
-	ans = 1 + rand() % 1000;
+	ans = 1 + rand() % RAND_RANGE;
 
 	printf("1~999の整数を当ててください。\n\n");
 
 	do {
-		printf("いくつかな: ");
+		printf("残り%d回 - いくつかな: ", remain);
 		scanf("%d", &no);
+		remain--;
 
 		if (no > ans) {
 			printf("\aもっと小さいよ。\n");
 		} else if (no < ans) {
 			printf("\aもっと大きいよ。\n");
 		} 
-	} while (no != ans);
+	} while (no != ans && remain > 0);
 
-	printf("正解です。\n");
+	if (no != ans) {
+		printf("\a残念。正解は%dでした。\n", ans);
+	} else {
+		printf("正解です。\n");
+		printf("%d回で正解しました。\n", MAX_STAGE - remain);
+	}
 
 	return 0;
 
